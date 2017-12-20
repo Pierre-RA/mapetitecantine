4b6f8bf7497d7445a4d88a6a8d89000237c3e44c
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2017-12-20 09:33:48 +0100

    feat: add google map

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,11 @@ export const environment = {
   production: true,
   facebookUrl: '/api/feed',
   wpBase: 'https://www.mapetitecantine.ch/wordpress/wp-json/wp/v2/',
-  galleryID: '2'
+  galleryID: '2',
+  GOOGLE_API_KEY: 'AIzaSyB2Aj947mMQNYSAxTaFgoTd42lwRoizxLU',
+  address: {
+    lat: 46.1673246,
+    lng: 6.1054392,
+    zoom: 14
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,11 @@ export const environment = {
   production: false,
   facebookUrl: 'http://demo6112386.mockable.io/feed',
   wpBase: 'https://www.mapetitecantine.ch/wordpress/wp-json/wp/v2/',
-  galleryID: '2'
+  galleryID: '2',
+  GOOGLE_API_KEY: 'AIzaSyB2Aj947mMQNYSAxTaFgoTd42lwRoizxLU',
+  address: {
+    lat: 46.1673246,
+    lng: 6.1054392,
+    zoom: 14
+  }
 };
