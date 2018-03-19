a92b381d8378a338019d9254fe0c8a5c9a1730eb
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2018-03-19 19:31:04 +0100

    fix: change environment url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   facebookUrl: '/api/feed',
-  wpBase: 'https://www.mapetitecantine.ch/wordpress/wp-json/wp/v2/',
+  wpBase: 'https://wordpress.mapetitecantine.ch/wordpress/wp-json/wp/v2/',
   galleryID: '2',
   GOOGLE_API_KEY: 'AIzaSyB2Aj947mMQNYSAxTaFgoTd42lwRoizxLU',
   address: {
